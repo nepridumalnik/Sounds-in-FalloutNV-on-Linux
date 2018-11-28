95a8ee27db9fe9af8f7c1fd5952963522ffaab85
Павел <44578700+nepridumalnik@users.noreply.github.com>
2018-11-28 23:00:56 +0700

    Update sound.py

--- sound.py
+++ sound.py
@@ -1,19 +1,15 @@
 import os
 from pydub import AudioSegment
 
-for root, dirs, files in os.walk("/home/muser/.local/share/Steam/steamapps/common/Fallout New Vegas enplczru/"):
+for root, dirs, files in os.walk("~/"): 
     for file in files:
         if file.endswith(".mp3"):
              text = os.path.join(root, file)
-             print(text[:-4]+'.wav')
              sound = AudioSegment.from_mp3(text)
              sound.export(text[:-4]+'.wav', format="wav")
-             
-for root, dirs, files in os.walk("/home/muser/.local/share/Steam/steamapps/common/Fallout New Vegas enplczru/"):
-    for file in files:
-        if file.endswith(".wav"):
-             text = os.path.join(root, file)
-             print(text[:-4]+'.mp3')
+             print(text[:-4]+'.wav'+' converted')
+             text = text[:-4]+'.wav'
              sound = AudioSegment.from_mp3(text)
              sound.export(text[:-4]+'.mp3', format="mp3")
+             print(text[:-4]+'.wav'+' deleted')
              os.remove(text)
